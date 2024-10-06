fae3d5837f4f6f2542fa1079039614a2e9397b58
左康生 <zhen0578@qq.com>
2024-10-06 20:24:44 +0800

    添加日志记录

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -5,5 +5,13 @@ module.exports = {
     instances: 1,
     autorestart: true,
     watch: false,
+    // 添加日志配置
+    log_date_format: "YYYY-MM-DD HH:mm:ss",
+    error_file: "./logs/realtime-error.log",
+    out_file: "./logs/realtime-out.log",
+    log_file: "./logs/realtime-combined.log",
+    merge_logs: true,
+    max_size: "10M",
+    max_files: 10
   }]
 };
